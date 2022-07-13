e48e8dde49d6ef5d64cac03a4a48d9c60ef7f3e5
David Matějček <david.matejcek@omnifish.ee>
2022-07-13 16:06:21 +0200

    Replaced usage of deprecated methods and constructs

--- src/main/java/org/codehaus/mojo/native2ascii/Native2Ascii.java
+++ src/main/java/org/codehaus/mojo/native2ascii/Native2Ascii.java
@@ -1,5 +1,6 @@
 /*
  * The MIT License
+ * Copyright (c) 2014-2022 MojoHaus
  * Copyright (c) 2007 The Codehaus
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the "Software"), to deal in the Software without restriction,
@@ -18,7 +19,6 @@ package org.codehaus.mojo.native2ascii;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.Closeable;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -27,16 +27,17 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.nio.CharBuffer;
 
-import org.apache.commons.lang3.text.translate.CodePointTranslator;
+import org.apache.commons.text.translate.CharSequenceTranslator;
 import org.apache.maven.plugin.logging.Log;
 
 /**
  * @author Evgeny Mandrikov
+ * @author David Matejcek
  */
 public final class Native2Ascii {
 
   private final Log log;
-  private final CodePointTranslator escaper;
+  private final CharSequenceTranslator escaper;
 
 
   /**
@@ -55,13 +56,13 @@ public final class Native2Ascii {
    * @return unicode escaped string
    */
   public String nativeToAscii(final String string) {
-    if (log.isDebugEnabled()) {
-      log.debug("Converting: " + string);
+    if (this.log.isDebugEnabled()) {
+      this.log.debug("Converting: " + string);
     }
     if (string == null) {
       return null;
     }
-    return escaper.translate(string);
+    return this.escaper.translate(string);
   }
 
 
@@ -74,36 +75,17 @@ public final class Native2Ascii {
    * @throws IOException
    */
   public void nativeToAscii(final File src, final File dst, final String encoding) throws IOException {
-    log.info("Converting: '" + src + "' to: '" + dst + "'");
-    BufferedReader input = null;
-    BufferedWriter output = null;
-    try {
-      if (!dst.getParentFile().exists()) {
-        dst.getParentFile().mkdirs();
-      }
-      input = new BufferedReader(new InputStreamReader(new FileInputStream(src), encoding));
-      output = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(dst), "ISO-8859-1"));
-
+    this.log.info("Converting: '" + src + "' to: '" + dst + "'");
+    if (!dst.getParentFile().exists()) {
+      dst.getParentFile().mkdirs();
+    }
+    try (BufferedReader input = new BufferedReader(new InputStreamReader(new FileInputStream(src), encoding));
+        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(dst), "ISO-8859-1"))) {
       final char[] buffer = new char[4096];
       int len;
       while ((len = input.read(buffer)) != -1) {
         output.write(nativeToAscii(CharBuffer.wrap(buffer, 0, len).toString()));
       }
-    } finally {
-      closeQuietly(src, input);
-      closeQuietly(dst, output);
     }
   }
-
-
-  private void closeQuietly(final File file, final Closeable closeable) {
-    if (closeable != null) {
-      try {
-        closeable.close();
-      } catch (final IOException e) {
-        log.warn("Could not close the file: " + file.getAbsolutePath());
-      }
-    }
-  }
-
 }
